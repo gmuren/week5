d10544e0ef1ed2ace1f7513b1552945452f4e879
Gabe Muren <gabe.muren@gmail.com>
2017-06-08 15:51:13 -0400

    mvp
    product works

---
+++ js/project.js
@@ -0,0 +1,21 @@
+var inputF;
+
+
+function calculateC() {
+	inputF = document.getElementById("inputFtoC").value;
+	var outputFtoC = (inputF - 32) * (5/9);
+	document.getElementById("solutionFtoC").innerHTML = outputFtoC.toFixed(2);
+ };
+
+document.getElementById("convertFtoC").onclick = calculateC;
+
+var inputC;
+
+function calculateF() {
+	inputC = document.getElementById("inputCtoF").value;
+	var outputCtoF = (inputC * 9/5) +32;
+	document.getElementById("solutionCtoF").innerHTML = outputCtoF.toFixed(2);
+ }
+
+document.getElementById("convertCtoF").onclick = calculateF;
+
